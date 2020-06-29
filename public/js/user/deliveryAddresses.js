d8e7e86c8b345895932861ea7e86cd3079f87123
XinYang-YXY <me@yxy.ninja>
2020-06-29 20:15:19 +0800

    Add address UI is done, working on

--- public/js/user/deliveryAddresses.js
+++ public/js/user/deliveryAddresses.js
@@ -1,19 +1,94 @@
-if(document.readyState == "loading"){
-    document.addEventListener("DOMContentLoaded", ready);
-} else{
-    ready();
+let placeSearch, autocomplete;
+
+let componentForm = {
+	street_number: "short_name",
+	route: "long_name",
+	locality: 'long_name',
+	administrative_area_level_1: "short_name",
+	postal_code: "short_name",
+	country: "long_name"
+
+}
+
+function initAutocomplete() {
+  
+	var defaultBounds = new google.maps.LatLngBounds(
+		new google.maps.LatLng(1.369, 103.8827),
+		new google.maps.LatLng(1.369, 103.8827)
+	)
+	let options = {
+		bounds: defaultBounds,
+		types: ['establishment']
+	}
+	
+	// Data input and output
+	let input = document.getElementById('autocomplete');
+	// Autocomplete magic spell API, takes in the input/output field and option which contains the bound
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+	autocomplete.setFields(['address_component'])
+
+
+	// When user selects an address from the drop-down, populate the address field
+	autocomplete.addListener('place_changed', fillInAddress)
+  console.log(autocomplete)
+
+}
+
+
+function fillInAddress(){
+  
+	let place = autocomplete.getPlace();
+	console.log(place, "place")
+
+	
+
+	for (let component in componentForm){
+		document.getElementById(component).value = '';
+		document.getElementById(component).disabled = false;
+	}
+
+
+	for (let i=0; i< place.address_components.length; i++){
+		console.log(place.address_components[i].short_name)
+		let type = place.address_components[i].types[0];
+		let inputField = document.getElementById(type);
+
+		if(componentForm[type]){
+			inputField.value = place.address_components[i][componentForm[type]]
+		}
+	
+	}
+
 }
 
 
 
 
-function ready(){
-  //Modal
-    // window.addToCartModal = document.getElementsByClassName("addToCartModal")[0];
-    // document.getElementsByClassName("closeBtn")[0].addEventListener("click", removeModal);
-    // window.addEventListener("click", clickOutsideModal);
-  //Modal
+//Onfocus, triggered when click the field
+function geolocate() {
+	if (navigator.geolocation) { // If geo is allowed
+		navigator.geolocation.getCurrentPosition((position) => {
+			let geolocation = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			console.log(geolocation)
+			console.log(position.coords.accuracy)
+			// The circle around the current location
+			let circle = new google.maps.Circle({center: geolocation, radius: position.coords.accuracy});
+			autocomplete.setBounds(circle.getBounds()); // Change the default coords to current
+
+		
+
+		})
+	}
 
 }
+
+
+
+
+
+
 
 
