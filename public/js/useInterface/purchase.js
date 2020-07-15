b8d351de3deae790445ebb4e57ef11b0c7f89a06
XJLim <limxiujia18@gmail.com>
2020-07-16 03:34:27 +0800

    edited Checkout page

--- public/js/useInterface/purchase.js
+++ public/js/useInterface/purchase.js
@@ -5,20 +5,8 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-    document.getElementById('openStripe').addEventListener('click', onStripe);
-    document.getElementById('closeStripe').addEventListener('click', offStripe);
-
     document.getElementById('openAddress').addEventListener('click', onAddress);
     document.getElementById('closeAddress').addEventListener('click', offAddress);
-}
-
-
-function onStripe(){
-    document.getElementById("overlayStripe").style.display = "block";
-}
-
-function offStripe(){
-    document.getElementById("overlayStripe").style.display = "none";
 }
 
 function onAddress(){
