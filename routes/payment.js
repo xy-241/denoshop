3a47d0408f9e56123ccfc63a422aa4d85a0dfa72
XJLim <limxiujia18@gmail.com>
2020-07-15 23:46:17 +0800

    Stripe working

--- routes/payment.js
+++ routes/payment.js
@@ -24,7 +24,6 @@ router.get("/cart", (req, res) => {
 });
 
 router.get("/checkout", (req, res) => {
-       
 	res.render("user/checkout", {
 		style: { text: "user/shopping/checkoutdelivery.css"},
 		title: "Checkout",
@@ -76,82 +75,67 @@ router.get("/useragreement", (req, res) => {
 });
 
 router.get("/orderplaced", (req, res) => {
+    console.log("Orderplaceddddd123");
+    console.log(req);
     res.render("user/orderplaced", {
         style: {text: "user/payment/ordersuccess.css"},
         title: "Order Success",
+        DeliveryAddress,
+        DeliveryEmail,
+        DeliveryPostal
     })
 })
 
-router.post("/purchaseStripe", (req, res) => {
+router.post("/charge", (req, res) => {
+
+    CartItem.findAll({
+        where: {
+            userId: req.user.id,
+        },
+    }).then((cartItems) => {
+        var items = []
+        cartItems.forEach(function(item) {
+            var itemdata = []
+            var quantity = item.quantity
+            var price = item.price
+            var id = item.id
+            itemdata.push({
+                product: id,
+                unit_amount: price,
+                currency: 'sgd',
+            })
+            items.push({
+                price_data: itemdata,
+                quantity: quantity,
+            })
+        })
+    });
+
+
+    let amount = 500;
+
+    console.log(req.body);
+    res.send('Test');
     
+    stripe.customers.update(
+        req.user.stripeId,
+        {source: req.body.stripeToken}
+    ).then(customer =>
+        stripe.charges.create({
+            amount,
+            description: "DenoShop",
+            currency: "sgd",
+            customer: customer.id,
+            })
+        ).then(charge => {
+        console.log(charge);
+        console.log("test");
+    })
+})
 
-    res.redirect("/payment/orderplaced");
-    // if (req.user.stripeid == null){
-    //     stripe.customers.create({
-            
-    //         address: [{
-    //             city: city,
-    //             line1: addressline1,
-    //             line2: addressline2,
-    //             postalcode: postalcode,
-    //             state: state,
-    //         }],
-    //         shipping: [{
-    //             address: shippingAddress,
-    //             name: shippingName,
-    //             phone: shippingPhone,
-    //         }]
-    //     }).then(customer => {
-    //         req.user.stripeid = customer.id
-    //     })
-    // }
-
-    // const expiry = document.getElementById('expDate').split('/')
-    // const expMonth = expiry[0]
-    // const expYear = expiry[1]
-
-    // CartItem.findAll({
-    //     where: {
-    //         userId: req.user.id,
-    //     },
-    // }).then((cartItems) => {
-    //     var items = []
-    //     cartItems.forEach(function(item) {
-    //         var itemdata = []
-    //         var quantity = item.quantity
-    //         var price = item.price
-    //         var id = item.id
-    //         itemdata.push({
-    //             product: id,
-    //             unit_amount: price,
-    //             currency: 'sgd',
-    //         })
-    //         items.push({
-    //             price_data: itemdata,
-    //             quantity: quantity,
-    //         })
-    //     })},
-
-    //     // stripe.tokens.create({
-    //     //     card: {
-    //     //         number: document.getElementById('cardNumber'),
-    //     //         exp_month: expMonth,
-    //     //         exp_year: expYear,
-    //     //         cvc: document.getElementById('cvv'),
-    //     //     },
-    //     //     function(err, token) {
-    //     //         if (err){
-    //     //             console.log(err.message)
-    //     //             res.status(err.code).end()
-    //     //         } else{
-    //     //             stripe.charges.create({
-    //     //                 amount: sum,
-    //     //                 currency: 'sgd',
-    //     //                 customer: req.user.stripeid,
-    //     //                 recipient_email: document.getElementById('email'),
-    //     //                 source: token,
-    //     //                 shipping: [{
-    //     //                     address: [{
+    
+    //     
+    
     //     //                         line1: document.getElementById('address'),
     //     //                         postal_code: document.getElementById('postal'),
     //     //                     }],
@@ -172,6 +156,6 @@ router.post("/purchaseStripe", (req, res) => {
     //     //     }
     //     // }),
     // );
-});
+// });
 
 module.exports = router;
