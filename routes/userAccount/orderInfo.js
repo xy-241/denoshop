496688d8032eb44b326be6f31034b026865066a0
XJLim <limxiujia18@gmail.com>
2020-07-24 03:59:46 +0800

    Removed irrelevant routes

--- routes/userAccount/orderInfo.js
+++ routes/userAccount/orderInfo.js
@@ -8,110 +8,18 @@ const ensureAuthenticated = require("../../helpers/auth");
 const Order = require("../../models/Order");
 // DB Table
 
-router.put("/update/:id", ensureAuthenticated, (req, res) => {
-    console.log(req.params.id)
-	DeliveryInfo.findOne({ where: { id: req.params.id } })
-		.then((deliveryAddr) => {
-			// Prevent unauthorised users
-			if (deliveryAddr.userId !== req.user.id) {
-				alertMessage(
-					res,
-					"danger",
-					"Unauthorised access to this user",
-					"fas fa-exclamation-circle",
-					true
-				);
-				req.logout();
-				res.redirect("/login");
-			} else if (deliveryAddr) {
-				let {
-					receiverName,
-					phoneNo,
-					country,
-					state,
-					city,
-					blockNo,
-					street,
-					unitNo,
-					postcode,
-				} = req.body;
-                let userId = req.user.id;
-                let updateObject = {
-                    receiverName,
-                    phoneNo,
-                    country,
-                    state,
-                    city,
-                    blockNo,
-                    street,
-                    unitNo,
-                    postcode,
-                    userId
-                }
-                console.log(updateObject)
-				DeliveryInfo.update(updateObject, { where: { id: req.params.id } })
-					.then((destroyInfo) => {
-						alertMessage(
-							res,
-							"info",
-							"Address Updated!",
-							"fas fa-trash-alt",
-							true
-						);
-						res.redirect("/account");
-					})
-					.catch((err) => console.log(err));
-			} else {
-				alertMessage(
-					res,
-					"info",
-					"Oops, something went wrong. Please try again later",
-					"fas fa-trash-alt",
-					true
-				);
-			}
-		})
-		.catch((err) => console.log(err));
+// View Order Details
+router.get("/view/:id", ensureAuthenticated, (req, res) => {
+	res.render("user/account", {
+		style: {
+			text: "user/management/account.css",
+			text1: "user/management/accountAddress.css",
+			text2: "user/management/accountOrder.css",
+		},
+		title: "My Order",
+		deliveryAddrs,
+		orders
+	});
 });
 
-// Delete delivery address
-router.get("/delete/:id", ensureAuthenticated, (req, res) => {
-	DeliveryInfo.findOne({ where: { id: req.params.id } })
-		.then((deliveryAddr) => {
-			// Prevent unauthorised users
-			if (deliveryAddr.userId !== req.user.id) {
-				alertMessage(
-					res,
-					"danger",
-					"Unauthorised access to this user",
-					"fas fa-exclamation-circle",
-					true
-				);
-				req.logout();
-				res.redirect("/login");
-			} else if (deliveryAddr) {
-				DeliveryInfo.destroy({ where: { id: req.params.id } })
-					.then((destroyInfo) => {
-						alertMessage(
-							res,
-							"info",
-							"Address deleted!",
-							"fas fa-trash-alt",
-							true
-						);
-						res.redirect("/account");
-					})
-					.catch((err) => console.log(err));
-			} else {
-				alertMessage(
-					res,
-					"info",
-					"Oops, something went wrong. Please try again later",
-					"fas fa-trash-alt",
-					true
-				);
-			}
-		})
-		.catch((err) => console.log(err));
-});
 module.exports = router;
