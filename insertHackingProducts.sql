5c0f1831c1d6292d1cf467e39895500a7cc724c5
XinYang Yu <47915643+XinYang-YXY@users.noreply.github.com>
2020-07-26 15:26:38 +0800

    👌IMPROVE: Update the product sql
    1) Change the source of images from personal cloud to cloudinary, to have a https connection
    2) Add in description for each item

--- insertHackingProducts.sql
+++ insertHackingProducts.sql
@@ -1,11 +1,11 @@
--- INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
--- VALUES ('1', '10', 'http://tinyurl.com/ycxvfan5', '2020-07-05 05:15:49', 'Deno Cap', 'Deno Cap', 'outfits', '20');
+INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
+VALUES ('1', '10', 'https://res.cloudinary.com/dchpyunul/image/upload/v1595087406/denoshop/hackingProducts/outfits/300-300-max_fuhw8x.jpg', '2020-07-05 05:15:49', 'Deno Cap', 'Elegant and simple cap empower your imagination.', 'outfits', '20');
 
--- INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
--- VALUES ('2', '40', 'http://tinyurl.com/y7xhusrj', '2020-07-05 05:15:49', 'Deno Hoodie', 'Deno Hoddie', 'outfits', '40');
+INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
+VALUES ('2', '40', 'https://res.cloudinary.com/dchpyunul/image/upload/v1595087407/denoshop/hackingProducts/outfits/300-300-maxout_kakwk7.jpg', '2020-07-05 05:15:49', 'Deno Hoodie', 'The hoodie you need to keep youself cozy while coding.', 'outfits', '40');
 
--- INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
--- VALUES ('3', '20', 'http://tinyurl.com/yb2z9wdb', '2020-07-05 05:15:49', 'Arduino', 'Arduino', 'tools', '240');
+INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
+VALUES ('3', '20', 'https://res.cloudinary.com/dchpyunul/image/upload/v1595087413/denoshop/hackingProducts/tools/300-300-maxar_wydfde.jpg', '2020-07-05 05:15:49', 'Arduino', 'The old but powerful micro-controller that every hacker should have one on their bell', 'tools', '240');
 
--- INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
--- VALUES ('4', '50', 'http://tinyurl.com/y8zhlj94', '2020-07-05 05:15:49', 'Raspberry Pi 4', 'Raspberyry Pi 4', 'tools', '430');
+INSERT INTO `denoshop`.`hackingProducts` (`id`, `price`, `imageFile`, `dateAdded`, `title`, `description`, `category`, `quantity`) 
+VALUES ('4', '50', 'https://res.cloudinary.com/dchpyunul/image/upload/v1595087415/denoshop/hackingProducts/tools/300-300-maxRa_vopg5q.jpg', '2020-07-05 05:15:49', 'Raspberry Pi 4', 'Powerful but affordable credit-card size computer we all love!', 'tools', '430');
