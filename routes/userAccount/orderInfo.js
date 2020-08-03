c0aa3833e9d68378fe108a168952605c5dbceb8e
XJLim <limxiujia18@gmail.com>
2020-08-04 02:47:19 +0800

    Added orderInfo route

--- routes/userAccount/orderInfo.js
+++ routes/userAccount/orderInfo.js
@@ -6,20 +6,44 @@ const ensureAuthenticated = require("../../helpers/auth");
 
 // DB Table
 const Order = require("../../models/Order");
+const PurchaseRecord = require("../../models/PurchaseRecord");
+const HackingProduct = require("../../models/HackingProduct");
 // DB Table
 
-// View Order Details
-router.get("/view/:id", ensureAuthenticated, (req, res) => {
-	res.render("user/account", {
-		style: {
-			text: "user/management/account.css",
-			text1: "user/management/accountAddress.css",
-			text2: "user/management/accountOrder.css",
+// Retrieve Order Items
+router.get("/retrieve/:id", ensureAuthenticated, (req, res) => {
+	let orderId = req.params.id
+	let userId = req.user.id
+
+	Order.findOne({
+		where: {
+			id: orderId,
+			userId: userId
 		},
-		title: "My Order",
-		deliveryAddrs,
-		orders
-	});
+		include: [PurchaseRecord]
+	}).then(async (order) => {
+		var purchaseRecordArr = order.purchaseRecords
+
+		var titleArr = []
+		for (let i = 0; i < purchaseRecordArr.length; i++) {
+			titleArr.push(purchaseRecordArr[i].title)
+		}
+
+		var prodDetails = await HackingProduct.findAll({
+			where: {
+				title: titleArr,
+			}
+		}).then((data) => {return data})
+
+		for (let i = 0; i < purchaseRecordArr.length; i++) {
+			var record = purchaseRecordArr[i]
+			var recordTitle = purchaseRecordArr[i].title
+			var recordFound = prodDetails.filter(function(item) { return item.title === recordTitle})
+			record["dataValues"]["imageFile"] = recordFound[0].imageFile
+			record["dataValues"]["price"] = recordFound[0].price
+		}
+		return res.json({order: order})
+	})
 });
 
 module.exports = router;
