b0697e8c88b1a7ac137d4e9990f6abfe2a69e875
Alanfy3 <alanthekurayami@gmail.com>
2020-08-14 16:26:05 +0800

    Added default values for ratings & stats model

--- models/ProductRatings.js
+++ models/ProductRatings.js
@@ -3,43 +3,55 @@ const db = require("../config/DBConfig");
 
 const ProductRatings = db.define("ProductRatings", {
 	year: {
-		type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
 	},
     jan: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     feb: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     mar: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     apr: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     may: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     jun: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     jul: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     aug: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     sep: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     oct: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     nov: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
     dec: {
         type: Sequelize.FLOAT,
+        defaultValue: 0,
     },
 });
 
--- models/ProductStats.js
+++ models/ProductStats.js
@@ -7,42 +7,55 @@ const ProductStats = db.define("productStats", {
 	},
     target: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     jan: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     feb: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     mar: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     apr: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     may: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     jun: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     jul: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     aug: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     sep: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     oct: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     nov: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
     dec: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
     },
 });
 
