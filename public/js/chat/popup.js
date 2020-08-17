fbeafea56d023712dca116e06f086daaba196c33
Chua Qihan <microkartz@gmail.com>
2020-08-17 11:53:56 +0800

    Css Fix

--- public/js/chat/popup.js
+++ public/js/chat/popup.js
@@ -2,7 +2,9 @@
     $(document).ready(function() {
         var $chatbox = $('.chatbox'),
             $chatboxTitleClose = $('.chatbox__title__close'),
+            $chatboxTitleClosex = $('.chatbox__title__closex'),
             $chatbotToggle = $('#chatbotToggle')
+            $chatbotTogglex = $('#chatbotTogglex')
         $chatboxTitleClose.on('click', function(e) {
             e.stopPropagation();
             $chatbox.removeClass('chatbox--open');
@@ -10,7 +12,21 @@
             $chatbotToggle.removeClass('toggle--open');
             $chatbotToggle.addClass('toggle--closed');
         });
+        $chatboxTitleClosex.on('click', function(e) {
+            e.stopPropagation();
+            $chatbox.removeClass('chatbox--open');
+            $chatbox.addClass('chatbox--closed');
+            $chatbotToggle.removeClass('toggle--open');
+            $chatbotToggle.addClass('toggle--closed');
+        });
         $chatbotToggle.on('click', function(e) {
+            e.stopPropagation();
+            $chatbox.removeClass('chatbox--closed');
+            $chatbox.addClass('chatbox--open');
+            $chatbotToggle.addClass('toggle--open');
+            $chatbotToggle.removeClass('toggle--closed');
+        });
+        $chatbotTogglex.on('click', function(e) {
             e.stopPropagation();
             $chatbox.removeClass('chatbox--closed');
             $chatbox.addClass('chatbox--open');
